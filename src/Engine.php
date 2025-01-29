4431f1effb550ae937adab6050e0ed89f22d1629
André Nascimento <andre.s.04.get.up@gmail.com>
2025-01-28 23:12:29 -0300

    feat: validate directories

--- src/Engine.php
+++ src/Engine.php
@@ -2,11 +2,15 @@
 
 namespace Andrexm\Astatine;
 
+use ErrorException;
+
 class Engine
 {
     private static string $views_path;
     private static string $cache_path;
+    private static string $extension;
     private static ?Engine $instance = null;
+    public static bool|string $errorMessage = false;
 
     private function __construct()
     {}
@@ -31,9 +35,38 @@ class Engine
      * @param string $cache_path
      * @return void
      */
-    public static function config(string $views_path, string $cache_path)
+    public static function config(string $views_path, string $cache_path, string $extension = ".php")
     {
         self::$views_path = $views_path;
         self::$cache_path = $cache_path;
+        self::$extension = $extension;
+    }
+
+    /**
+     * Validate views and cache directories
+     *
+     * @return boolean
+     */
+    public static function validateDirectories(): bool
+    {
+        // Verify views directory
+        try {
+            if (!is_dir(self::$views_path))
+                throw new ErrorException("Failed to read views directory!");
+        } catch (ErrorException $err) {
+            self::$errorMessage = $err->getMessage();
+            return false;
+        }
+
+        // Verify cache directory
+        try {
+            if (!is_dir(self::$cache_path))
+                throw new ErrorException("Failed to read cache directory!");
+        } catch (ErrorException $err) {
+            self::$errorMessage = $err->getMessage();
+            return false;
+        }
+
+        return true;
     }
 }
