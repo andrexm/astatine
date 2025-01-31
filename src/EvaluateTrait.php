fdaa1d7a5ca62f03906a147372c5276eba25afaa
André Nascimento <andre.s.04.get.up@gmail.com>
2025-01-30 22:35:04 -0300

    feat: generate view

---
+++ src/EvaluateTrait.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Andrexm\Astatine;
+
+trait EvaluateTrait
+{
+    /**
+     * Starts processing the view
+     *
+     * @param string $content
+     * @return string
+     */
+    static private function generate(string $content): string
+    {
+        return $content;
+    }
+
+}
